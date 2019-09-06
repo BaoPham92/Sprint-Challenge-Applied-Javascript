6894de6ef93727ceb7ac5478de33a0e138c756e1
Cuza <baopham92@yahoo.com>
2019-09-06 12:47:31 -0400

    Initial commit

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,44 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const header = () => {
+    // Create Element
+    const createElement = (type, selector = undefined, content = undefined) => {
+        const element = document.createElement(type)
+
+        // If class, add
+        if (selector) {
+            element.classList.add(selector)
+        }
+
+        // If content, add
+        if (content) {
+            element.textContent = content
+        }
+
+        return element;
+    }
+    
+    // Header
+    const header = createElement('header', 'header')
+
+    // Span
+    const span = createElement('span', 'date', 'SMARCH 28, 2019')
+    const span2 = createElement('span', 'temp', '98°')
+
+    // H1
+    const h1 = createElement('h1', undefined, 'Lambda Times')
+
+    // Append
+    header.append(span)
+    header.append(h1)
+    header.append(span2)
+
+    // Log data
+    console.log(header)
+
+    return header
+}
+
+// Grab target and
+document.querySelector('.header-container').append(header());
